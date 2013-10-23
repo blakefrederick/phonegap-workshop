c71ecd9a0f608069d64b419c172af9e2acfa3a20
Blake <blakefre@gmail.com>
2013-10-23 13:34:23 -0700

    oranges

--- js/main.js
+++ js/main.js
@@ -1,26 +1,14 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+	findByName: function() {
+		var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
+		});
+	},
 	
 	renderHomeView: function() {
-		var html =
-				"<div class='header'><h1>Home</h1></div>" +
-				"<div class='search-view'>" +
-				"<input class='search-key'/>" +
-				"<ul class='employee-list'></ul>" +
-				"</div>"
-		$('body').html(html);
+		$('body').html(this.homeTpl());
 		$('.search-key').on('keyup', $.proxy(this.findByName, this));
 	},
 	
@@ -31,6 +19,8 @@ var app = {
 		});
 		
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     },
 	
 	showAlert: function (message, title) {
